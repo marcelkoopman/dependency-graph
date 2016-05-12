e051a3c85ee52ec92eff5e8b051fc7f2a4c7bfcb
koopman.m <koopman.m@tkppensioen.nl>
2016-05-12 11:55:51 +0200

    fixes

--- src/test/java/com/github/marcelkoopman/NeoGraphFacadeTest.java
+++ src/test/java/com/github/marcelkoopman/NeoGraphFacadeTest.java
@@ -1,12 +1,14 @@
 package com.github.marcelkoopman;
 
 import iot.jcypher.query.JcQueryResult;
+import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,6 +16,8 @@ import static org.junit.Assert.assertEquals;
  * Created by marcel on 29-4-16.
  */
 public class NeoGraphFacadeTest {
+
+    private static final String RESOURCE_PATH = "src\\test\\resources\\";
 
     @Test
     public void testOneGraph() throws IOException, XmlPullParserException {
@@ -25,7 +29,8 @@ public class NeoGraphFacadeTest {
     @Test
     public void testTwoGraphs() throws IOException, XmlPullParserException {
         final NeoGraphFacade facade = new NeoGraphFacade();
-        final JcQueryResult result = facade.createGraphs(Arrays.asList(new File("pom.xml"), new File("wgp.xml"), new File("wgpadmin.xml"), new File("giservices.xml")));
+        final List<File> files = Arrays.asList(new File(RESOURCE_PATH + "csv.xml"), new File(RESOURCE_PATH + "wgp.xml"), new File(RESOURCE_PATH + "wgpadmin.xml"));
+        final JcQueryResult result = facade.createGraphs(files);
         assertEquals(result.getJsonResult().toString(), 0, result.getDBErrors().size());
     }
 }
